12028c2dc043dee710f0166e050337c912d2e995
sungkwonkudo <sungkwonkudo@gmail.com>
2018-03-21 11:46:39 -0400

    fixed some things

--- src/main/java/com/revature/hydra/screening/service/ScreeningCompositionMessagingService.java
+++ src/main/java/com/revature/hydra/screening/service/ScreeningCompositionMessagingService.java
@@ -1,10 +1,7 @@
 package com.revature.hydra.screening.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonObject;
@@ -18,15 +15,12 @@ public class ScreeningCompositionMessagingService {
 
 	private static final String SINGLE_TRAINEE_ROUTING_KEY = "JyoH3uRmktGn9MnW";
 	
-	private static final Logger log = LoggerFactory.getLogger(ScreeningCompositionMessagingService.class);
-	
 	/**
 	 * RabbitMQ Exchange Name
 	 * 
 	 */
 	private static final String RABBIT_REPO_EXCHANGE = "revature.hydra.repos";
 	
-	@Scheduled(fixedDelay = 3000L)
 	public SimpleTrainee sendSingleTraineeRequest(Integer traineeId) {
 		JsonObject traineeRequest = new JsonObject();
 		
@@ -36,7 +30,6 @@ public class ScreeningCompositionMessagingService {
 		SimpleTrainee st =  (SimpleTrainee)rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE,
 				SINGLE_TRAINEE_ROUTING_KEY,
 				traineeRequest.toString());
-		log.info(st.toString());
 		return st;
 	}
 	
